08d7ffb0bc1872f6bbcf278fe7c2c36b6d49849c
1obotomy <94368907+1obotomy@users.noreply.github.com>
2025-10-01 14:14:56 +0300

    Update reminders.js

--- backend/reminders.js
+++ backend/reminders.js
@@ -8,13 +8,16 @@ export function addReminder(text, dateTime, callback) {
   const now = Date.now();
   const delay = targetTime - now;
 
+  console.log(`Добавлено напоминание: ${text} на ${dateTime}, delay=${delay}`);
+
   if (delay > 0) {
     setTimeout(() => {
       reminder.status = "done";
+      console.log(`Срабатывает напоминание: ${text}`);
       callback();
     }, delay);
   } else {
-    reminder.status = "done"; // если время уже прошло
+    reminder.status = "done";
   }
 }
 
